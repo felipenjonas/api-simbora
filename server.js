724d9383b96c190110ed9859646e01b7f6ef7df5
Felipe Jonas <felipe.gabriel.novais.jo@gmail.com>
2020-07-13 19:50:34 -0300

    try 3 deploy

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 const route = require('./src/routes');
+const bodyParser = require('body-parser');
 const port = 9000
 
 const app = express();
 
+
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(route);
